bf75b40d2ae1de76bb0d49894cf394fd50d9f290
lgainsbrook <lauren.gainsbrook@gmail.com>
2018-04-08 01:18:46 -0400

    add js to send student # and uni

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-let student_num = document.getElementById('student_num').value;
-let university = document.getElementById('select_uni').value;
+function send_data() {
+    let student_num = document.getElementById('student_num').value;
+    let university = document.getElementById('select_uni').value;
+    console.log(student_num, university);
+}
