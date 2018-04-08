bef9b8f1341ca515fd693184161094c85e3f3fd9
lgainsbrook <lauren.gainsbrook@gmail.com>
2018-04-08 10:43:20 -0400

    add message to respond to transcript request

--- js/script.js
+++ js/script.js
@@ -10,9 +10,22 @@
 
 
 function send_data() {
-    let student_num = document.getElementById('student_num').value;
-    let university = document.getElementById('select_uni').value;
+    // let student_num = document.getElementById('student_num').value;
+    // let university = document.getElementById('select_uni').value;
+    document.getElementById("disappear1").style.display = 'none';
+    document.getElementById("student_num").style.display = 'none';
+    document.getElementById("disappear2").style.display = 'none';
+    document.getElementById("select_uni").style.display = 'none';
+    remove_button();
+
+
 }
+function remove_button() {
+    document.getElementById("button").style.display = 'none';
+    document.getElementById("appear").style.display = 'block';
+
+}
+
 
 
 
