18c1a45f6a8b67ee362701082bc032553d0a8cb8
lgainsbrook <lauren.gainsbrook@gmail.com>
2018-04-08 04:31:15 -0400

    add js function for uni side- work-in-progress

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function send_data() {
-    let student_num = document.getElementById('student_num').value;
+    const student_num = document.getElementById('student_num').value;
     let university = document.getElementById('select_uni').value;
     console.log(student_num, university);
 }
---
+++ uni.js
@@ -0,0 +1,5 @@
+function send_transcript() {
+    document.getElementById('student').innerHTML = student_num;
+}
+
+document.getElementById('student').innerHTML = student_num;
